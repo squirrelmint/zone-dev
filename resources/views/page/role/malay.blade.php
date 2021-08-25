865760d0a3e682d7922d334f37d0b406a7c1d9ba
Joe <66766658+ghost9425@users.noreply.github.com>
2021-08-25 21:34:16 +0700

    edit section_role

--- resources/views/page/role/malay.blade.php
+++ resources/views/page/role/malay.blade.php
@@ -1,5 +1,5 @@
 @extends('layout.header')
-@section('yeekeelotto')
+@section('content')
     <div class="col-12 col-md-9">
         <div class="h-100 zone-background zone-reward zone-content">
             <img src="{{asset('img/reward/banner-yeekee.png')}}">
